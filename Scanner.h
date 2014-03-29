64fee0fc39324dae17a9d74f95f8b62c03e6cc25
emilymarie <emilymarie@pacbell.net>
2014-03-29 16:59:57 -0700

    names added

--- Scanner.h
+++ Scanner.h
@@ -4,6 +4,11 @@
 //
 //  Created by Bryce Holton.
 //
+//  Updated by:
+//	Ashley Krueger (alkruege), Mathew Scott Dexheimer (PickelBarelKumqueat), 
+//	Savannah Pucket (snpuckett), Emily Falkner(emfalkne, or emilymarie)
+//	GitHub Url: https://github.com/Thelandofsunshine/sunshineandrainbows
+
 
 #ifndef Lab4_Scanner_h
 #define Lab4_Scanner_h
