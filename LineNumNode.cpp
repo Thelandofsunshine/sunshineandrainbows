889c17a03cf92a665e353df2333530b93aec35ca
alkruege <alkruege@asu.edu>
2014-04-07 10:50:59 -0700

    added names... again

--- LineNumNode.cpp
+++ LineNumNode.cpp
@@ -1,3 +1,8 @@
+//  Created by:
+//	Ashley Krueger (alkruege), Mathew Scott Dexheimer (sdexh, or PickelBarelKumqueat),
+//	Savannah Pucket (snpuckett), Emily Falkner(emfalkne, or emilymarie)
+//	GitHub Url: https://github.com/Thelandofsunshine/sunshineandrainbows
+
 #include "LineNumNode.h"
 
 
